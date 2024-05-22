cb74ee7b07d18dc3dd98340251b07bb7a025a0e6
MeDeveloperWeb <www.shivachaurasia@gmail.com>
2024-05-23 00:23:09 +0530

    Feat: Responsive Nav Bar

--- src/app/_components/NavLink.jsx
+++ src/app/_components/NavLink.jsx
@@ -1,22 +1,83 @@
+'use client';
+
 import Link from 'next/link';
+import menu from '@/assets/menu.svg';
+import { useRef } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { usePathname } from 'next/navigation';
 
 export default function NavLink() {
+  const modalRef = useRef(null);
+  const path = usePathname();
+  console.log(path);
+
+  const navList = [
+    {
+      name: 'Home',
+      path: '/'
+    },
+    {
+      name: 'Our Services',
+      path: '/services'
+    },
+    {
+      name: 'About Us',
+      path: '/about'
+    },
+    {
+      name: 'Contact Us',
+      path: '/contact'
+    }
+  ];
+
   return (
     <nav>
-      <ul className="flex h-full justify-evenly gap-8">
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/services">Services</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
+      <ul className="hidden md:flex h-full justify-evenly gap-8">
+        {navList.map((item) => (
+          <li
+            key={item.name + 'lg'}
+            className={
+              item.path === path ? 'pointer-events-none text-slate-600' : ''
+            }
+          >
+            <Link href={item.path}>{item.name}</Link>
+          </li>
+        ))}
       </ul>
+
+      <div className="md:hidden">
+        <button
+          onClick={() => {
+            if (modalRef.current.hasAttribute('open')) modalRef.current.close();
+            else modalRef.current.show();
+          }}
+        >
+          <Image src={menu} alt="menu" width={24} height={24} />
+        </button>
+      </div>
+      <dialog
+        ref={modalRef}
+        onClick={() => modalRef.current.close()}
+        className="mr-4 mt-4 card rounded-sm"
+      >
+        <ul className="flex flex-col">
+          {navList.map((item) => (
+            <li key={item.name + 'sm'}>
+              <Link
+                href={item.path}
+                className={`py-2 px-8 w-full block ${
+                  item.path === path
+                    ? 'pointer-events-none text-slate-600'
+                    : 'hover:bg-slate-700 hover:text-white'
+                }`}
+              >
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </dialog>
     </nav>
   );
 }
